6e76b26e7ce8d149d2b0dbcd83f15efd33a503c7
Brad Morris <bradmorris@Brads-Air.attlocal.net>
2023-07-03 15:10:09 -0700

    project complete

--- car/car.js
+++ car/car.js
@@ -1,1 +1,18 @@
 // Build a Car class!
+class Car {
+    constructor(make, model) {
+        this.make = make,
+        this.model = model
+        this.odometer = 0
+    }
+    
+    drive(miles) {
+        this.odometer += miles
+    }
+    async driveAsync(miles) {
+        this.odometer += miles
+        return this.odometer
+    }
+}
+
+module.exports = Car;
--- car/car.spec.js
+++ car/car.spec.js
@@ -1,1 +1,49 @@
+const Car = require('./car')
+
+
 // Test away!
+describe('our first tests', () => {
+    test('sanity', () => {
+        expect(5).toBe(5)
+    })
+})
+
+describe('Car Class', () => {
+    
+    beforeEach(() => {
+        prius = new Car('toyota', 'prius')
+    })
+
+
+    test('it is defined', () => {
+        expect(Car).toBeDefined()
+        expect(Car).toBeInstanceOf(Function)
+    })
+    test('has model and make', () => {
+        expect(prius).toHaveProperty('make', 'toyota') // 2nd argument = make value
+        expect(prius).toHaveProperty('model')
+        expect(prius.make).toBeDefined() // open ended, make is simply defined
+        expect(prius.model).toEqual('prius') // equal to the correct value
+        expect(prius.model).toBe('prius')
+        expect(prius).toMatchObject({ make: 'toyota', model: 'prius' })
+    })
+    test("new cars start with odometer at 0", () => {
+        expect(prius).toHaveProperty('odometer', 0)
+    })
+    test("cars have a drive method", () => {
+        expect(prius.drive).toBeDefined()
+        expect(prius.drive).toBe(Car.prototype.drive)
+    })
+    test('drive async', () => {
+        prius.drive(10)
+        expect(prius.odometer).toBe(10)
+        prius.drive(5)
+        expect(prius.odometer).toBe(15)
+    })
+    test("Test async method", async () => {
+        let updatedO = await prius.driveAsync(10)
+        expect(prius.odometer).toBe(10)
+        updatedO = await prius.driveAsync(5)
+        expect(prius.odometer).toBe(15)
+    })
+})
